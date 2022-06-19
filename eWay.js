e55e7fbcb5cede9b23a5bc5578d5ab6f634509c6
Lucas Iarley <lucasiarleys.oliveira@gmail.com>
2022-06-19 10:05:28 -0300

    update

--- eWay.js
+++ eWay.js
@@ -9,8 +9,8 @@ const {CepBrasil,
  
  //introduzindo o pack correiros-brasil na função concultarCep
 const {consultarCep} = require('correios-brasil');
-const cep = "48900028";//o Cep que eu quero
-
+const cep = 48900028 ;//o Cep que eu quero
+// document.querySelector('.rastrear').value
 consultarCep(cep).then((response)=>{
 	console.log(response);
 });
@@ -18,12 +18,14 @@ consultarCep(cep).then((response)=>{
 
 /*---------como rastrear uma encomenda --------*/
 const {rastrearEncomendas} = require('correios-brasil');
+//
 
 let codRastreio = ["JN257125105BR"];
 //Array de códigos de rastreio. Pode ter mais que um item e pode ter outros formatos
 
 rastrearEncomendas(codRastreio).then((response) =>{
 	console.log(response);
+	
 })
 
 
